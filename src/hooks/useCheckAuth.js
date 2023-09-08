ed77601a87ccac55b1a1a4dd299147b90c420290
Christian R. Lamadrid <christiancamiloriveralamadrid@gmail.com>
2023-09-08 09:47:06 -0500

    Update useCheckAuth.js

--- src/hooks/useCheckAuth.js
+++ src/hooks/useCheckAuth.js
@@ -1,7 +1,7 @@
 import { onAuthStateChanged } from "firebase/auth";
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { login, logout } from "../store/auth/authslice";
+import { login, logout } from "../store/auth/authSlice";
 import { FirebaseAuth } from "../firebase/config";
 import { startLoadingPosts } from "../store/findy/findyThunks";
 
